cb1fa9cbe28b8213016dc15163b7d236373a8006
Adivesh <adiveshsanagi99@gmail.com>
2025-05-08 23:20:03 +0530

    onr file was deleted called python from 2demo

---
+++ example/expython.py
@@ -0,0 +1,5 @@
+print("This is my second trial on making project code and pushing it into GitHub")
+
+for i in range(0, 20):
+    print(i)
+    i = i + 1  # This line is unnecessary; `for` loop automatically increments
